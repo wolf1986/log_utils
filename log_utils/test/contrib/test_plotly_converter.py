5e690b2bf898bad08cd7067cf65013321694ac24
Willy Fenchenko <Wolf1986@gmail.com>
2018-12-22 12:06:12 +0200

    [Tests] Collected all tests under test folder [Tests] Fixed temp directory not being deleted after test is done

--- log_utils/test/contrib/test_plotly_converter.py
+++ log_utils/test/contrib/test_plotly_converter.py
@@ -1,3 +1,5 @@
+import shutil
+
 import numpy as np
 import logging
 from pathlib import Path
@@ -8,6 +10,7 @@ from log_utils.data_logger import DataLogger
 
 from log_utils.data_logger.contrib.plotly_converter import PlotlyConverter, PlotlyFigure
 from log_utils.data_logger.handlers import SaveToDirHandler
+from log_utils.helper import LogHelper
 
 
 class TestPlotlyConverter(TestCase):
@@ -44,26 +47,32 @@ class TestPlotlyConverter(TestCase):
         """
 
         path_dir_logs = Path(mkdtemp())
-        logger = DataLogger('TestLogger', logging.DEBUG)
-        logger.setLevel(logging.INFO)
-        data_handler = SaveToDirHandler(path_dir_logs)
-        data_handler.addConverter(PlotlyConverter())
-        logger.addHandler(data_handler)
 
-        meshes = {'i': np.array([2, 2]), 'showscale': False, 'opacity': 0.3, 'k': np.array([0, 1]),
-                  'z': np.array([1., 1., 1., 1.], dtype=np.float32), 'name': '',
-                  'y': np.array([0., 0., 1., 1.], dtype=np.float32),
-                  'colorscale': [[0, 'rgb(6, 236, 35)'], [1.0, 'rgb(6, 236, 35)']],
-                  'x': np.array([0., 1., 0., 1.], dtype=np.float32), 'type': 'mesh3d', 'j': np.array([1, 3]),
-                  'reversescale': False,
-                  'intensity': np.array([-0., -0.13533528, -0., -0.04978707], dtype=np.float32)
-                  }
-        logger.info('Temp Message', data=PlotlyFigure(data=[meshes], layout=self.get_default_grid_settings("test")))
-        file_found = False
-        for file in path_dir_logs.iterdir():
-            if "html" in file.name:
-                file_found = True
-        self.assertTrue(file_found)
+        try:
+            logger = DataLogger('TestLogger', logging.INFO)
+
+            logger.addHandler(SaveToDirHandler(path_dir_logs).addConverter(PlotlyConverter()))
+            logger.addHandler(LogHelper.generate_color_handler())
+
+            meshes = {'i': np.array([2, 2]), 'showscale': False, 'opacity': 0.3, 'k': np.array([0, 1]),
+                      'z': np.array([1., 1., 1., 1.], dtype=np.float32), 'name': '',
+                      'y': np.array([0., 0., 1., 1.], dtype=np.float32),
+                      'colorscale': [[0, 'rgb(6, 236, 35)'], [1.0, 'rgb(6, 236, 35)']],
+                      'x': np.array([0., 1., 0., 1.], dtype=np.float32), 'type': 'mesh3d', 'j': np.array([1, 3]),
+                      'reversescale': False,
+                      'intensity': np.array([-0., -0.13533528, -0., -0.04978707], dtype=np.float32)
+                      }
+            logger.info(
+                'Plotly figure sample',
+                data=PlotlyFigure(data=[meshes], layout=self.get_default_grid_settings("test"))
+            )
+
+            self.assertTrue(
+                len(list(path_dir_logs.glob('*.html'))) == 1
+            )
+
+        finally:
+            shutil.rmtree(str(path_dir_logs))
 
 
 if __name__ == '__main__':
