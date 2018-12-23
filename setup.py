9d72eb8be9bc14ef432d1f3f3426a1a2f49053b6
Willy Fenchenko <Wolf1986@gmail.com>
2018-12-23 12:58:54 +0200

    [Setup] Fixed missing package in setup.py & updated version

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ setup(
     author='Willy Polychenko',
     author_email='wolf1986@gmail.com',
     url='https://github.com/wolf1986/log_utils',
-    version='0.3.3',
+    version='0.3.4',
     test_suite='nose.collector',
     tests_require=['nose'],
-    packages=['log_utils', 'log_utils/data_logger'],
+    packages=[
+        'log_utils',
+        'log_utils/data_logger',
+        'log_utils/data_logger/contrib'
+    ],
     install_requires=['colorama', 'colorlog']
 )
